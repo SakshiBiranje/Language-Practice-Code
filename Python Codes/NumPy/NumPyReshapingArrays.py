c0b261e387e661698f018e7e5a0a75948edcf37a
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-03-05 12:44:48 +0530

    Update

--- Python Codes/NumPy/NumPyReshapingArrays.py
+++ Python Codes/NumPy/NumPyReshapingArrays.py
@@ -3,7 +3,6 @@
 #The outermost dimension will have 4 arrays, each with 3 elements:
 
 import numpy as np
-
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 
 newarr = arr.reshape(4, 3) #(rows,cols)
