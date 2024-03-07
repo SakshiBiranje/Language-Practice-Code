1eb0500c24691de5b0e8cb7614f2fa0b9ad84bae
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-03-07 12:51:04 +0530

    Update

--- Python Codes/NumPy/NumPyReshapingArrays.py
+++ Python Codes/NumPy/NumPyReshapingArrays.py
@@ -8,7 +8,6 @@ arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 newarr = arr.reshape(4, 3) #(rows,cols)
 
 print(newarr)
-
 print("\n")
 
 #2 arrays that contains 3 arrays, each with 2 elements
