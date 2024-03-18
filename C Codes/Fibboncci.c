a7c06481d0693f31603e48c356b701eb7c8700e7
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-03-18 13:12:23 +0530

    Update

--- C Codes/Fibboncci.c
+++ C Codes/Fibboncci.c
@@ -4,7 +4,6 @@ void fibonacci(int n) {
     int first = 0, second = 1, next;
 
     printf("Fibonacci series up to %d numbers:\n", n);
-
     for (int i = 0; i < n; i++) {
         if (i <= 1) {
             next = i;
