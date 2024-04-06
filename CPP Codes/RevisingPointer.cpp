60af4ff84e5143099015f16c531572c925a026a9
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-04-06 18:20:23 +0530

    Update

--- CPP Codes/RevisingPointer.cpp
+++ CPP Codes/RevisingPointer.cpp
@@ -7,7 +7,6 @@ int main()
     int a = 4;
     int *ptr= &a;
  	int *array = new int[3];
-
     cout << "Value of a: " << a << endl;
     cout << "Address of a: " << &a << endl;
     cout << "Value of ptr: " << ptr << endl;
