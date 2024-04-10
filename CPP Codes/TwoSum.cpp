b986820e35307cc269408c6064ffd1311902bc96
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-04-10 19:31:07 +0530

    Update

--- CPP Codes/TwoSum.cpp
+++ CPP Codes/TwoSum.cpp
@@ -9,7 +9,6 @@ vector<int> twoSum(vector<int>& nums, int target) {
     
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
-        
         // Check if complement exists in the map
         if (numMap.find(complement) != numMap.end()) {
             return { numMap[complement], i };
