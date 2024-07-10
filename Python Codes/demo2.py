5594b68f103af3b040235cb21cb1870bc364789c
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-07-10 19:54:04 +0530

    update

---
+++ Python Codes/demo2.py
@@ -0,0 +1,23 @@
+#variables in python 
+student = "Sam"
+print(student)
+
+#data types - int , float , complex, boolean ,string
+a1=10
+print(a1)
+type(a1)
+b1=3.14
+print(b1)
+type(b1)
+c1= "sakshi"
+print(c1)
+type(c1)
+d1= 4+2j
+print(d1)
+type(d1)
+
+#arithmetic operations
+a=10
+b=20
+
+print(a+b)
