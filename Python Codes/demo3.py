4bedd0ef87470b36573e46043e6738b65054e39c
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-07-11 19:40:56 +0530

    updated

---
+++ Python Codes/demo3.py
@@ -0,0 +1,20 @@
+#Logical operator
+a=True
+b=False
+print(a|b)
+print(a&b)
+print(b|a)
+print(a|a)
+
+#tokens , keywords , string
+str1= "hello world !!!"
+print(str1)
+my_string = "My name is Sakshi."
+print(my_string[0])
+print(my_string[16])
+print(my_string)
+print(my_string.lower())
+print(my_string.upper())
+print(my_string.replace('Sakshi','Radha'))
+print(my_string.find('is'))  #satrting index of that word
+print(my_string.split('i'))
