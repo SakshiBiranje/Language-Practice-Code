24d364ed55cc20a7ba9509a15e5553de3d567049
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-07-11 19:53:52 +0530

    update

--- Python Codes/demo4.py
+++ Python Codes/demo4.py
@@ -6,3 +6,16 @@ print(tup1[0])
 print(tup1[-1])
 print(tup1[0:2])
 print(len(tup1))
+
+l1=[1, 'Radha', 3.14,True]
+print(l1)
+print([0])
+print([1])
+print(l1.append(-87.999))
+print(type(l1))
+l1[0]=100
+print(l1)
+l1.pop()
+print(l1)
+l1.reverse()
+print(l1)
