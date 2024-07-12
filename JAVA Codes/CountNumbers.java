4b1dcfae7d5f744f86be62e50bbbaf4eadcdc824
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-07-12 18:31:31 +0530

    Update

--- JAVA Codes/CountNumbers.java
+++ JAVA Codes/CountNumbers.java
@@ -2,6 +2,7 @@ public class CountNumbers
 {
     public static void main(String args[])
     {
+        
         int num=123456;
         int count= String.valueOf(num).length();
         System.out.println(count);
