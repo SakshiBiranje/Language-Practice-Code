2a6f76b2582d122070f31d11607def6980948a0e
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-07-17 11:50:26 +0530

    update

---
+++ Python Codes/ifelse.py
@@ -0,0 +1,16 @@
+# statements- if -else
+a=10
+b=20
+if a>b:
+    print("a is greater than b")
+else :
+    print("b is greater than a")
+
+c=30
+if(a>b) & (a>c):
+    print("a is greatest")
+elif(b>a) & (b>c):
+    print("b is greatest")
+else :
+    print("c is greatest")
+
