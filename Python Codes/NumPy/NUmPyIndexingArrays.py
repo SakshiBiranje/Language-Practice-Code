02ff51e4af28fd3dd01fbbdc16fa03e306a25b48
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-07-21 14:35:16 +0530

    update

--- Python Codes/NumPy/NUmPyIndexingArrays.py
+++ Python Codes/NumPy/NUmPyIndexingArrays.py
@@ -1,4 +1,5 @@
 #indexing of arrays
+
 import numpy as np
 
 arr = np.array([1, 2, 3, 4])
@@ -8,11 +9,12 @@ print(arr[2])
 print(arr[1] + arr[2])
 
 #print particular element from matrix
+
 arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
 
 print('2nd element on 1st row: ', arr[0, 1])
 
-#print 
+#print the third element of the second array of the first array
 arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
 
 print(arr[0, 1, 2])
