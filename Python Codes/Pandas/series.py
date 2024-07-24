0a45d6127c5d84b06ece26a3b193692a4a6fdc5d
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-07-24 20:13:57 +0530

    update

--- Python Codes/Pandas/series.py
+++ Python Codes/Pandas/series.py
@@ -46,3 +46,12 @@ myvar = pd.Series(calories)
 
 print(myvar)
 
+#Create a Series using only data from "day1" and "day2":
+
+import pandas as pd
+
+calories = {"day1": 420, "day2": 380, "day3": 390}
+
+myvar = pd.Series(calories, index = ["day1", "day2"])
+
+print(myvar)
