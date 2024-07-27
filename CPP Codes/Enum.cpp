3aa37385907c9f9eb0ef3007550932dd340e36fb
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-07-27 22:42:02 +0530

    Added Code

---
+++ CPP Codes/Enum.cpp
@@ -0,0 +1,11 @@
+#include<iomanip>
+#include<iostream>
+using namespace std;
+int main()
+{
+    enum Meal {Breakfast,Lunch,Dinner};
+    cout<<Breakfast<<endl;
+    cout<<Lunch<<endl;
+    cout<<Dinner<<endl;
+    return 0;
+}
