ce1b8e5cee176d3087b6112ddecaa7c722be1cef
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-07-27 22:40:35 +0530

    Added Code

---
+++ C Codes/Array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int array[5];
+    int i;
+    for(i=0;i<5;i++)
+    {
+        printf("Enter the value of array[%d]:",i);
+        scanf("%d",&array[i]);
+    }
+    for(i=0;i<5;i++)
+    {
+        printf("%d\n",array[i]);
+    }
+    return 0;
+}
