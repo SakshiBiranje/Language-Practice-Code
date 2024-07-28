1a3813bedbc11697cdf32db31b77afca3661b7af
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-07-28 17:43:39 +0530

    update

---
+++ Python Codes/Pandas/CleaningEmptyCells.py
@@ -0,0 +1,35 @@
+#Return a new Data Frame with no empty cells:
+
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+new_df = df.dropna()
+
+print(new_df.to_string())
+
+#Remove all rows with NULL values:
+
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+df.dropna(inplace = True)
+
+print(df.to_string())
+
+#Replace NULL values with the number 130:
+
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+df.fillna(130, inplace = True)
+
+#Replace NULL values in the "Calories" columns with the number 130:
+
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+df["Calories"].fillna(130, inplace = True)
