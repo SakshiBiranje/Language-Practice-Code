3edf153d6c4183444c1bebf5392ec197db012a02
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-07-29 20:29:36 +0530

    Update

--- JAVA Codes/Remove_Element.java
+++ JAVA Codes/Remove_Element.java
@@ -1,39 +1,38 @@
 import java.util.Scanner;
 
-public class Remove_Element{
-public int Remove_ele(int[] nums, int v){
-int k=0;
-for(int x: nums){
-if(x != nums[]){
-nums[k]=nums[i];
-k++;
-}
-}
-return k;
-} 
-public static void main(String[] args){
-Scanner sc = new Scanner();
+public class Remove_Element {
 
-System.out.println("Enter size of array: ");
-int n=sc.nextInt();
-int[] nums =new int[n];
-System.out.println("Enter array elements: ");
-for(int i=0;i<n;i++)
-{
-	nums[i]=sc.nextInt();
-}
-System.out.println("Enter element to remove: ");
-int val =sc.nextInt();
+    public int Remove_ele(int[] nums, int v) {
+        int k = 0;
+        for (int x : nums) {
+            if (x != v) {
+                nums[k++] = x;
+            }
+        }
+        return k;
+    }
 
-Remove_Element sc = new Remove_Element();
-int l= sc.Remove_element(nums,val);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
 
-System.out.println("New length of array : "+l)
-System.out.println("Modified array: ");
-for(int i=0 ;i<l ;i++)
-{
-System.out.println(nums[i]+" ");
-}
+        System.out.println("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter array elements: ");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println("Enter element to remove: ");
+        int val = sc.nextInt();
 
-}
+        Remove_Element remover = new Remove_Element();  
+        int newLength = remover.Remove_ele(nums, val);
+
+        System.out.println("New length of array: " + newLength);  
+        System.out.println("Modified array: ");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(nums[i] + " ");  
+        }
+        sc.close(); 
+    }
 }
