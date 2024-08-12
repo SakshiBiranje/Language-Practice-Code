2ef3fdbba9104163ee8217b8339bbb896d47e0e9
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-08-12 10:10:24 +0530

    Added Code

--- C Codes/2DArray.c
+++ C Codes/2DArray.c
@@ -2,7 +2,13 @@
 
 int main() {
   int matrix[2][3] = { {1, 4, 2}, {3, 6, 8} };
-  printf("%d", matrix[0][2]);
- 
+
+  int i, j;
+  for (i = 0; i < 2; i++) {
+    for (j = 0; j < 3; j++) {
+      printf("%d\n", matrix[i][j]);
+    }
+  }
+  
   return 0;
 }
