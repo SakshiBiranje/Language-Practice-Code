f0e6be1661f04d2c1652cd6bf5518e1b9547a99a
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-08-20 19:55:43 +0530

    added code

---
+++ C Codes/FibonacciSeries.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+void fibboncci(int n)
+{
+    if(n<=0)
+    {
+        printf("Invalid input");
+    }
+    int a=0,b=1,next;
+    for(int i=0;i<n;i++)
+    {
+        if(i==0)
+        {
+            printf("%d\n",a);
+            continue;
+        }
+        if(i==1)
+        {
+            printf("%d\n",b);
+            continue;
+        }
+        next=a+b;
+        a=b;
+        b=next;
+        printf("%d\n",next);
+    }
+    printf("\n");
+}
+int main() {
+    int s;
+    printf("Enter value:");
+    scanf("%d",&s);
+    fibboncci(s);
+    return 0;
+}
