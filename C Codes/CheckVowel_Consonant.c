00ab77fcb354ebf4b958e5412eb239a64a2fbd0b
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-09-06 21:27:56 +0530

    Update CheckVowel_Consonant.c

--- C Codes/CheckVowel_Consonant.c
+++ C Codes/CheckVowel_Consonant.c
@@ -6,6 +6,7 @@ int main()
     int c;
     printf("Enter a character : ");
     scanf("%c",&c);
+    
     if(c=='a'|| c=='e'|| c=='i'|| c=='o'|| c=='u'||c=='A'|| c=='E'|| c=='I'|| c=='O'|| c=='U')
     {
         printf("vowel");
