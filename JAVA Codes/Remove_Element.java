d69e298ae06a63b314f485178fe5490e2bf99963
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-09-09 12:13:14 +0530

    added code

--- JAVA Codes/Remove_Element.java
+++ JAVA Codes/Remove_Element.java
@@ -1,38 +1,39 @@
 import java.util.Scanner;
-import java.util.*;
 
-class Remove_Element {
-    public int removeElement(int[] nums, int val) {
-        int k = 0;
-        for (int x : nums) {
-            if (x != val) {
-                nums[k++] = x;
-            }
-        }
-        return k;
-    }
+public class Remove_Element{
+public int Remove_ele(int[] nums, int v){
+int k=0;
+for(int x: nums){
+if(x != nums[]){
+nums[k]=nums[i];
+k++;
+}
+}
+return k;
+} 
+public static void main(String[] args){
+Scanner sc = new Scanner();
+
+System.out.println("Enter size of array: ");
+int n=sc.nextInt();
+int[] nums =new int[n];
+System.out.println("Enter array elements: ");
+for(int i=0;i<n;i++)
+{
+	nums[i]=sc.nextInt();
+}
+System.out.println("Enter element to remove: ");
+int val =sc.nextInt();
+
+Remove_Element sc = new Remove_Element();
+int l= sc.Remove_element(nums,val);
+
+System.out.println("New length of array : "+l)
+System.out.println("Modified array: ");
+for(int i=0 ;i<l ;i++)
+{
+System.out.println(nums[i]+" ");
 }
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of elements in the array: ");
-        int n = scanner.nextInt();
-        int[] nums = new int[n];
-        System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < n; i++) {
-            nums[i] = scanner.nextInt();
-        }
-        System.out.print("Enter the value to remove: ");
-        int val = scanner.nextInt();
-
-        Remove_Element sb = new Remove_Element();
-        int newLength = sb.removeElement(nums, val);
-
-        System.out.println("New length of the array: " + newLength);
-        System.out.println("Modified array:");
-        for (int i = 0; i < newLength; i++) {
-            System.out.print(nums[i] + " ");
-        }
-    }
+}
 }
