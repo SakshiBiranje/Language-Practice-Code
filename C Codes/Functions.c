bb3eb3932be3802eef12331153e2405fbeaa35ec
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-09-09 21:31:09 +0530

    Update Functions.c

--- C Codes/Functions.c
+++ C Codes/Functions.c
@@ -6,10 +6,12 @@ void calculateSum()
   int x = 5;
   int y = 10;
   int sum = x + y;
+
   printf("The sum of x + y is: %d", sum);
 }
 
-int main() {
+int main() 
+{
   calculateSum();  // call the function
   return 0;
 }
