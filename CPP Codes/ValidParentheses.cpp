c7325d574bcadf4cd7d8ca92d9268ab124964180
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-09-12 20:20:20 +0530

    update

---
+++ CPP Codes/ValidParentheses.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution 
+{
+public:
+    bool isValid(const string& s) 
+    {
+        string stk;
+        for (char c : s) 
+        {
+            if (c == '(' || c == '{' || c == '[') 
+            {
+                stk.push_back(c);
+            } else if (stk.empty() || !match(stk.back(), c)) 
+            {
+                return false;
+            } else {
+                stk.pop_back();
+            }
+        }
+        return stk.empty();
+    }
+
+private:
+    bool match(char l, char r) 
+    {
+        return (l == '(' && r == ')') ||
+               (l == '[' && r == ']') ||
+               (l == '{' && r == '}');
+    }
+};
+
+int main() 
+{
+    Solution solution;
+    string test ;
+   
+    cout<<"Enter parentheses : "<<endl;
+    cin>>test;
+
+    bool result = solution.isValid(test);
+    cout<<"Result is : "<<(result ? "true": "false")<<endl;
+
+    return 0;
+}
