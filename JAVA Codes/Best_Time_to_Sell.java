6723f602b089bd90fc6a8ade7e40588a2317394c
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-09-15 06:29:17 +0530

    update

--- JAVA Codes/Best_Time_to_Sell.java
+++ JAVA Codes/Best_Time_to_Sell.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
-
 class Best_Time_to_Sell {
-    public int MaxProfit(int[] prices) {
-        int ans=0, min = prices[0];
-        for(int value : prices )
-        {
+public int maxprofit(int[] prices) {
+    int ans=0, min = prices[0];
+    for(int value : prices )
+    {
         ans=Math.max(ans,value-min);
         min=Math.min(min , value);
-        }
-        return ans;
+    }
+    return ans;
 }
-
-public static void main(String[] args) {
+public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int[] prices= new int[7];
     System.out.println("Enter prices of 7 days :");
-    for(int i=0;i<7;i++) 
-    {
+    for(int i=0;i<7;i++){
          prices[i]=sc.nextInt();
     }
     Best_Time_to_Sell obj = new Best_Time_to_Sell();
-    int ans= obj.Maxprofit(prices);
+    int ans= obj.maxprofit(prices);
     System.out.println("profit :"+ans);
 }
 }
