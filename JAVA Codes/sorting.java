9fb40e877f2439b0ff25a313753653668333c12b
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-09-23 19:51:27 +0530

    Update

--- JAVA Codes/sorting.java
+++ JAVA Codes/sorting.java
@@ -1,6 +1,6 @@
 import java.util.*;
-
-public class sorting{
+public class sorting
+{
     public static void printArray(int arr[]) {
         for(int i =0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
