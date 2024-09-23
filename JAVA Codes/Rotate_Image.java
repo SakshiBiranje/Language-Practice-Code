abbf2c9681fd415b2dd70f6a903c0b8425f069d1
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-09-23 19:50:49 +0530

    Update

---
+++ JAVA Codes/Rotate_Image.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+class Rotate_Image{
+    // Function to rotate the matrix
+    public void rotate(int[][] matrix) {
+        int s = 0, n = matrix.length;
+        while (s < (n >> 1)) {
+            int e = n - s - 1;
+            for (int i = s; i < e; ++i) {
+                int t = matrix[i][e];
+                matrix[i][e] = matrix[s][i];
+                matrix[s][i] = matrix[n - i - 1][s];
+                matrix[n - i - 1][s] = matrix[e][n - i - 1];
+                matrix[e][n - i - 1] = t;
+            }
+            ++s;
+        }
+    }
+
+    // Function to print the matrix
+    public void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input the size of the matrix
+        System.out.print("Enter the size of the matrix (n x n): ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        // Input the matrix elements
+        System.out.println("Enter the matrix elements row by row:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Display the original matrix
+        System.out.println("Original Matrix:");
+        Rotate_Image sol = new Rotate_Image();
+        sol.printMatrix(matrix);
+
+        // Rotate the matrix
+        sol.rotate(matrix);
+
+        // Display the rotated matrix
+        System.out.println("Rotated Matrix:");
+        sol.printMatrix(matrix);
+
+        sc.close();
+    }
+}
