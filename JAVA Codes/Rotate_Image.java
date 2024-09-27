3cf0b88716f2ec655c403bdb0bdb78752fd31259
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-09-27 19:01:37 +0530

    Update

--- JAVA Codes/Rotate_Image.java
+++ JAVA Codes/Rotate_Image.java
@@ -26,8 +26,7 @@ class Rotate_Image{
         }
     }
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the size of the matrix (n x n): ");
