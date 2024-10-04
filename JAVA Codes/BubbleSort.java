be2014bbe2c074b47c0f120cb234ae6d4981b31a
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-10-04 08:45:13 +0530

    Update

--- JAVA Codes/BubbleSort.java
+++ JAVA Codes/BubbleSort.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
-public class BubbleSort{
+public class BubbleSort
+{
     public static void printArray(int arr[]){
         for(int i=0 ; i<arr.length ; i++){
             System.out.print(arr[i]+" ");
