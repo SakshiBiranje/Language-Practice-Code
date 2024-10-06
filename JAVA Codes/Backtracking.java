bffc506dccfb5e15a84262ca1cd1ead991adc076
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-10-06 09:06:58 +0530

    Update

--- JAVA Codes/Backtracking.java
+++ JAVA Codes/Backtracking.java
@@ -1,11 +1,12 @@
 public class Backtracking 
 {
-    public static void printPermutation(String str,String permutation,int idx) {
+    public static void printPermutation(String str,String permutation,int idx) 
+    {
         if(str.length() == 0){
             System.out.println(permutation);
             return;
         }
-        
+    
         for(int i=0;i<str.length();i++){
             char currChar = str.charAt(i);
             String newStr = str.substring(0, i)+str.substring(i+1);
