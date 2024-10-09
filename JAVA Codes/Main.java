fe619936cd551c54d328157d0ca3acb0e575cf94
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-10-09 21:02:43 +0530

    Update Main.java

--- JAVA Codes/Main.java
+++ JAVA Codes/Main.java
@@ -14,7 +14,8 @@ class Remove_Element
     }
 }
 
-public class Main {
+public class Main 
+{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of elements in the array: ");
