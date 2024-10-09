0f5facf18bfa57f6d2b7b73d00ad6076828e20ad
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-10-09 21:02:15 +0530

    Update HollowRectanglePattern.java

--- JAVA Codes/HollowRectanglePattern.java
+++ JAVA Codes/HollowRectanglePattern.java
@@ -5,7 +5,6 @@ public class HollowRectanglePattern
     public static void main(String[] args) 
     {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the number of rows: ");
         int rows = scanner.nextInt();
         System.out.print("Enter the number of columns: ");
