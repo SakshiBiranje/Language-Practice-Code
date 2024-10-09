a8544a832a6082b6e2af01d34d3e1b64f6d57a44
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-10-09 21:02:34 +0530

    Update number.java

--- JAVA Codes/number.java
+++ JAVA Codes/number.java
@@ -7,7 +7,8 @@ public class number
         Scanner scanner = new Scanner(System.in);
         int n=5;
         int number = 1;
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=n;i++)
+        {
             for(int j=1;j<=i;j++){
                 System.out.print(number+" ");
                 number++;
