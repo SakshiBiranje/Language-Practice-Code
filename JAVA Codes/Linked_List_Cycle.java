d6025843954ca756f0d019eaef216a071d60eebd
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-10-25 17:39:35 +0530

    Update

---
+++ JAVA Codes/Linked_List_Cycle.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
+        next = null;
+    }
+}
+
+public class Linked_List_Cycle{
+    
+}
