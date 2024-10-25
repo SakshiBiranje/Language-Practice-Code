a3f8b107a810dfe0b977f0d8b05ca76258ef6b45
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-10-25 19:22:28 +0530

    Update

--- JAVA Codes/rectangle.java
+++ JAVA Codes/rectangle.java
@@ -2,16 +2,16 @@ import java.util.*;
 public class rectangle
 {
     public static void main(String args[]) {
-        Scanner sc=new Scanner(System.in);
+        Scanner scanner =new Scanner(System.in);
         
         System.out.print("i:");
         int i =scanner.nextInt();
         System.out.print("j:");
         int j =scanner.nextInt();
 
-        for(int i=1;i<=4;i++) {
+        for(i=1;i<=4;i++) {
             //inner loops
-            for(int j=1;j<=5;j++) {
+            for( j=1;j<=5;j++) {
                 if(i == 1 || j == 1 || i == n || j == m){
                 System.out.print("*");
             }else{
