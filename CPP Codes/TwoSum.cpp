9162976d0dcbf6bea6e084d001d2953fbdc42947
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-11-04 21:12:06 +0530

    Update TwoSum.cpp

--- CPP Codes/TwoSum.cpp
+++ CPP Codes/TwoSum.cpp
@@ -3,7 +3,8 @@
 #include <unordered_map>
 using namespace std;
 
-vector<int> twoSum(vector<int>& nums, int target) {
+vector<int> twoSum(vector<int>& nums, int target) 
+{
     unordered_map<int, int> numMap;
     
     for (int i = 0; i < nums.size(); i++) {
