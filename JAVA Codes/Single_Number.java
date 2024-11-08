d1cd6e56c7fd102eb41a45abc0a0cb52e95d4d66
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-11-08 15:13:14 +0530

    Update

--- JAVA Codes/Single_Number.java
+++ JAVA Codes/Single_Number.java
@@ -7,15 +7,17 @@ class Single_Number
         int ans=0;
         for(int value : nums)
         {
-            ans ^= value; //XOR operator
+            ans ^= value;  //XOR operator
         }
         return ans;
     }
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter Array size: ");
         int size = sc.nextInt();
+
         int[] nums = new int[size];
         System.out.print("Enter elements : ");
         for(int i=0;i<size;i++)
@@ -24,6 +26,7 @@ class Single_Number
         }
         Single_Number obj = new Single_Number();
         int ans = obj.SingleNumber(nums);
+        
         System.out.println("Single Number : "+ ans);
     }
 }
