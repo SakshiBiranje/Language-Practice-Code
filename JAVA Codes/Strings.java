c7dbff75c5d4d6b1c927afbf36564bd80fc83471
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-11-09 13:45:40 +0530

    Update

--- JAVA Codes/Strings.java
+++ JAVA Codes/Strings.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class Strings 
 {
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
         System.out.println("Your name is "+name);
