fe10ca6f72d1e6cc3de26e3fb2d50ca5cdc82d5d
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-11-23 08:37:55 +0530

    Update

--- JAVA Codes/ReverseNumber.java
+++ JAVA Codes/ReverseNumber.java
@@ -3,7 +3,8 @@ public class ReverseNumber
  public static void main(String[] args) 
  { 
     int num=12345, reversed = 0; 
-    while (num != 0) {
+    while (num != 0) 
+    {
          reversed =reversed* 10+ num % 10; 
          num /= 10;
         } 
