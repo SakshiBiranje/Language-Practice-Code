a99d03834d3c94b5993850ff870edb45a75c7416
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-11-23 08:37:51 +0530

    Update

--- JAVA Codes/RecursionTowerOfHanoi.java
+++ JAVA Codes/RecursionTowerOfHanoi.java
@@ -1,4 +1,5 @@
-public class RecursionTowerOfHanoi { 
+public class RecursionTowerOfHanoi 
+{ 
     public static void TowerOfHanoi(int n,String src,String helper,String dest){
        if(n==1){
         System.out.println("transfer disk "+ n + " from " + src + " to "+dest);
