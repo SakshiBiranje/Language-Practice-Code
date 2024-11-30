37d10f4859f07cf1d85f32fe2e321790452564e9
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-11-30 17:28:51 +0530

    Update

--- JAVA Codes/SumCalculator.java
+++ JAVA Codes/SumCalculator.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-public class SumCalculator {
+public class SumCalculator 
+{
     
     public static double calculateSum(double num1, double num2) {
         return num1 + num2;
