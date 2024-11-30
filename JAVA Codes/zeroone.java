0ad3de45a23a59b85654fab671b441e449f329c5
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-11-30 17:29:04 +0530

    Update

--- JAVA Codes/zeroone.java
+++ JAVA Codes/zeroone.java
@@ -4,6 +4,7 @@ public class zeroone
 {
 public static void main(String args[]){
     //Scanner scanner = new scanner(System.in);
+    
     int n = 5;
     for(int i =1; i <= n;i++){
         for(int j=1;j<=i;j++){
