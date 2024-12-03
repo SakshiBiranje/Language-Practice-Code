cb8ce7064a77fcfcb35d0715c36fb78e39bf1804
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-03 19:04:00 +0530

    Update

--- JAVA Codes/Solidrhombus.java
+++ JAVA Codes/Solidrhombus.java
@@ -5,8 +5,7 @@ public class Solidrhombus
     {
         int n =5;
         for(int i=1;i<=n;i++)
-        {
-            
+        { 
             //spaces
             for(int j=1;j<=n-i;j++) 
             {
