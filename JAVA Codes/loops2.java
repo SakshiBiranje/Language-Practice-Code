c0c687c638eccd3a78f54a5a3f924d44400e9edf
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-04 14:01:23 +0530

    Update

--- JAVA Codes/loops2.java
+++ JAVA Codes/loops2.java
@@ -1,12 +1,16 @@
 import java.util.*;
-public class loops2 {
-    public static void main(String args[]) {
+public class loops2 
+{
+    public static void main(String args[])
+     {
         int n = 4;
         int m = 5;
         //outer loop
-        for(int i=1;i<=n;i++) {
+        for(int i=1;i<=n;i++) 
+        {
             //inner loops
-            for(int j=1;j<=m;j++){
+            for(int j=1;j<=m;j++)
+            {
                 System.out.print("*");
             }
             System.out.println();
