c343b28e028e27d759f7a96678d617a9f392b943
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-05 15:26:13 +0530

    Update

--- JAVA Codes/Recursion_fibonacci.java
+++ JAVA Codes/Recursion_fibonacci.java
@@ -1,13 +1,17 @@
-public class Recursion_fibonacci {
-    public static void printFib(int a , int b ,int n){
-    if(n==0){
+public class Recursion_fibonacci 
+{
+    public static void printFib(int a , int b ,int n)
+    {
+    if(n==0)
+    {
         return;
     }
     int c=a+b;
     System.out.println(c);
     printFib(b,c,n-1);
     }
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         int a=0 ;
         int b=1;
         
