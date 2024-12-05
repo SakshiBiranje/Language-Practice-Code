09b940581e45ee682033495c79baef411bcd2c08
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-05 15:25:33 +0530

    Update

--- JAVA Codes/Recursion1.java
+++ JAVA Codes/Recursion1.java
@@ -1,15 +1,18 @@
-public class Recursion1{
-    //to print 1 to 5
-    public static void printNum(int n){
-       if(n==0){
-        return;
-       }
+import java.util.*;
+public class Recursion1
+{
+    public static void printnum(int n)
+    {
+        if( n==0){
+            return;
+        }
         System.out.println(n);
-        printNum(n-1);
+        printnum(n-1);
     }
-    public static void main(String args[]){
-       int n=5;
-       printNum(n); 
-
+    
+    public static void main(String[] args)
+    {
+        int n=5;
+        printnum(n);
     }
 }
