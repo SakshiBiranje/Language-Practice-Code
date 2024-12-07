2193922a0c1363d45150e66de678cdff6d1b7642
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-07 10:20:14 +0530

    Update

---
+++ JAVA Codes/UnaryOperators.java
@@ -0,0 +1,15 @@
+public class UnaryOperators{
+    public static void main(String[] args){
+        int a= 10;
+        int b= 10;
+
+        System.out.println("PostIncrement : "+ (a++));
+        System.out.println("Preincrement : "+ (++a));
+        System.out.println("Postdecrement : "+ (b--));
+        System.out.println("Predecrement : " + (--b));
+
+    }
+}
+//assignment operators : += , -=, *= , /= , <<= , >>= etc
+//relational operators :  != , == , >= , <= , < , > they return boolean results i.e true or false 
+//logical operator : ! logical NOT , && logical AND, || logical OR
