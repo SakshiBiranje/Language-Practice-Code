b8192e586b06d3a93a3f28a37af2bac0f54a8203
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-07 10:20:43 +0530

    Update

---
+++ JAVA Codes/ArithmeticoperatorOnString.java
@@ -0,0 +1,14 @@
+public class ArithmeticoperatorOnString{
+    //arithmetic operators on string
+    public static void main (String[] args){
+        String num1 = "13";
+        String num2 = "25";
+
+        double a1= Integer.parseInt(num1);
+        double a2 = Integer.parseInt(num2);
+        System.out.println("Addition : "+ (a1+a2));
+        System.out.println("division : "+ (a1/a2));
+        System.out.println("Modulo : "+ (a1%a2));
+        System.out.println("Multiplication : "+ (a1*a2));
+    }
+}
