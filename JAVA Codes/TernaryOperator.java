f2209bfbfd57dfa2bb6511cded6a2a108645073b
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-07 10:20:29 +0530

    Update

---
+++ JAVA Codes/TernaryOperator.java
@@ -0,0 +1,13 @@
+public class TernaryOperator
+{
+    //syntax : condition ? if true : if false
+    public static void main (String[] args)
+    {
+    int a = 20 , b = 10 , c = 30 , result;
+    
+    result= ((a > b) ? (a > c) ? a : c :(c > b) ? b : c);
+    System.out.println("Max of three : "+ result); 
+    }
+}
+//bitwise operator : & , | , ^, ~
+// shift operator : a>>1, a <<1
