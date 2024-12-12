ebfe4be2399eb9bb3bd5958160989d811c27d516
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-12 20:48:07 +0530

    Create DoWhile1.java

---
+++ JAVA Codes/DoWhile1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class DoWhile1{
+    public static void main(String[] args)
+    {
+        int c=1;
+        do {
+            System.out.println("Hello !!");
+            c++;
+        }
+        // here while condition is false still there will be 1 iteration 
+        // so it will print o/p for 1 time...
+        while(c>=3);
+    }
+}
