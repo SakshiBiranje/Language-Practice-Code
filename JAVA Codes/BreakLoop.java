5aa7beb23663dcb8189ca0017148ab64e8d205f6
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-12 20:48:27 +0530

    Create BreakLoop.java

---
+++ JAVA Codes/BreakLoop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class BreakLoop{
+    public static void main(String[] args){
+        for(int i= 0;i<9;i++){
+            if(i==5)
+            break;
+            System.out.println("Hello " + i);
+        }
+        System.out.println("Loop completed !!");
+    }
+}
