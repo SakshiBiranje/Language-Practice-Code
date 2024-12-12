2a0bc49f297c04048929b3cfb00475064bd9ec56
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-12 20:48:38 +0530

    Update Reverse_String.java

--- JAVA Codes/Reverse_String.java
+++ JAVA Codes/Reverse_String.java
@@ -1,12 +1,12 @@
 //Reverse a String
 import java.util.*;
-public class Reverse_String
-{
+public class Reverse_String {
 public static void main(String[] args)
 {
-String str = "Automation";
-StringBuilder reversed = new StringBuilder(str).reverse();
 
-System.out.println(reversed);
+   String str = "Automation";
+   StringBuilder reversed = new StringBuilder(str).reverse();
+
+   System.out.println(reversed);
    }
 }
