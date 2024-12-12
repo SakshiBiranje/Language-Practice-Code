b1da86a30888323c4c63f4642aef6a374b3c606c
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-12 20:48:36 +0530

    Create ContinueKeyword.java

---
+++ JAVA Codes/ContinueKeyword.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class ContinueKeyword 
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 10; i++) 
+        {
+            // Skip condition where i==3
+            if (i == 3) 
+                continue;
+            // Exit the loop when i == 7
+            if (i == 7) 
+                break; 
+            System.out.print(i + " ");
+        }
+    }
+}
