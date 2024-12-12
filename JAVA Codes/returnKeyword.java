e644fde06871281cbed142f9cb6ad4eafa59945e
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-12 20:48:13 +0530

    Create returnKeyword.java

---
+++ JAVA Codes/returnKeyword.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class returnKeyword
+{
+    double SS(double a, double b)
+    {
+        double avg = 0;
+        avg = (a + b) / 2.0;
+        return avg;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(new returnKeyword().SS(5.5, 6.5));
+    }
+}
