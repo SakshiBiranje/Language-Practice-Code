d072ea04de28e3a53d2dc784ad2ce0e8be445973
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-12 20:47:51 +0530

    Create BreakStatement.java

---
+++ JAVA Codes/BreakStatement.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class BreakStatement{
+    // using switch ,case, break keywords
+    public static void main(String[] args){
+    int n=1;
+    switch(n){
+        case 1 : 
+        System.out.println("Sakshi");
+        break;
+        case 2 : 
+        System.out.println("Santosh");
+        break;
+        case 3 : 
+        System.out.println("Biranje");
+        break;
+    } 
+}
+}
