e1df9598ad2c90f34a8daf213e852680d760086e
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-15 21:53:37 +0530

    Create String1.java

---
+++ JAVA Codes/String1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class String1{
+    public static void main(String[] args){
+        String str = new String("Hi , there!!");
+        System.out.println(str);
+    }
+}
