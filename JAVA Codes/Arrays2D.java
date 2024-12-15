cf4f85f5d750c59f5de6c38738189ff0ecdf9b6a
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-15 21:53:35 +0530

    Update Arrays2D.java

--- JAVA Codes/Arrays2D.java
+++ JAVA Codes/Arrays2D.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 public class Arrays2D {
-    public static void main(String args[]) {
+    public static void main(String args[]) 
+    {
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
         int col = sc.nextInt();
