8bcb6c7a930bee8923ef25df6198a094071848ac
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-15 21:53:40 +0530

    Create String2.java

---
+++ JAVA Codes/String2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class String2{
+    public static void main(String[] args){
+        String s = "Tony";
+        s.concat("Stark");
+        // op will be Tony
+        System.out.println(s);
+        s= s.concat(" Stark");
+        System.out.println(s);
+    }
+}
