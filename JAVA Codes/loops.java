3e54d124a8d833a09a54d47e8176733421e4317f
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-16 12:12:53 +0530

    Update loops.java

--- JAVA Codes/loops.java
+++ JAVA Codes/loops.java
@@ -6,7 +6,6 @@ public class loops{
   //for(int i=0;i<4;i++){
   //System.out.println("Hello World");
   //}
-
       //print num from 0 to 10
     //for(int i=0;i<11;i++){
     //System.out.println(i);
