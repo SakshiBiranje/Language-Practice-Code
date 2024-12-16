715d60272d0d4aa87209f82a107a5068d4227977
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-16 12:13:01 +0530

    Update Recursion2.java

--- JAVA Codes/Recursion2.java
+++ JAVA Codes/Recursion2.java
@@ -10,8 +10,7 @@ public class Recursion2 {
         printNum(n+1);
     }
     
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
        int n=1;
        printNum(n); 
 
