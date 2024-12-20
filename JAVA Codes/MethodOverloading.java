d0d94b1087ddfcb4eac127a033bd11c571602747
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-20 12:06:43 +0530

    Create MethodOverloading.java

---
+++ JAVA Codes/MethodOverloading.java
@@ -0,0 +1,23 @@
+class MethodOverloading{
+
+    public int add(int n1 , int n2, int n3){
+        return n1 + n2 + n3;
+    }
+    public int add(int n1 , int n2){
+        return n1 + n2 ;
+    }
+    public double add(double n1 , int n2, int n3){
+        return n1 + n2 ;
+    }
+
+    // all 3 methods have same name but pararmeters and its types is different 
+    // so it will work for all possible parameter type inputs
+    public static void main(String[] args){
+
+        MethodOverloading obj = new MethodOverloading();
+       // int result = obj.add(3,4,5);
+      //  int result = obj.add(3,4);
+        int result = obj.add(10 , 20, 15);
+        System.out.println(result);
+    }
+}
