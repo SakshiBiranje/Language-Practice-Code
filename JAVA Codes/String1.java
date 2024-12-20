adf748c50c4de1566c0e1324826c5de49df721e8
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-20 12:06:59 +0530

    Update String1.java

--- JAVA Codes/String1.java
+++ JAVA Codes/String1.java
@@ -3,6 +3,14 @@ import java.util.*;
 public class String1
 {
     public static void main(String[] args){
+        /*
+         * String immutability means that once a string object is created, 
+         * its value cannot be changed. Any modification results in the creation of a new string.
+         * Strings are immutable for security, performance, and thread safety reasons.
+         * It prevents unwanted changes and helps optimize memory usage.
+
+
+         */
         String str = new String("Hi , there!!");
         System.out.println(str);
     }
