2a5881e8267b4831ad13074308c987998e003183
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-22 20:11:19 +0530

    Update Move_Zeros.java

--- JAVA Codes/Move_Zeros.java
+++ JAVA Codes/Move_Zeros.java
@@ -21,7 +21,8 @@ class MoveZeros {
     
         int[] nums = new int[n];
         System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             nums[i] = sc.nextInt();
         }
         
