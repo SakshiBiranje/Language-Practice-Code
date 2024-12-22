df6db2414a8c3081340e0bbb661254329cb42f50
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-22 20:13:13 +0530

    Update invertedhalfpyramidwithno.java

--- JAVA Codes/invertedhalfpyramidwithno.java
+++ JAVA Codes/invertedhalfpyramidwithno.java
@@ -5,6 +5,7 @@ public class invertedhalfpyramidwithno
     public static void main(String[] args) 
     {
         int n=6;
+        
         for(int i=n;i>=1;i--)
         {
             for(int j=1;j<=n;j++)
