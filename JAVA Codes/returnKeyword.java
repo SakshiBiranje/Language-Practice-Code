5f89cbdfdc0ea48ff646deec5c493773bb4c1dba
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-23 12:27:17 +0530

    Update returnKeyword.java

--- JAVA Codes/returnKeyword.java
+++ JAVA Codes/returnKeyword.java
@@ -8,6 +8,7 @@ class returnKeyword
         avg = (a + b) / 2.0;
         return avg;
     }
+    
     public static void main(String[] args)
     {
         System.out.println(new returnKeyword().SS(5.5, 6.5));
