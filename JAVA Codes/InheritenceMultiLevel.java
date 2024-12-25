7ad400c695feae406e11e672bb584fba894cad83
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-25 16:41:07 +0530

    Create InheritenceMultiLevel.java

---
+++ JAVA Codes/InheritenceMultiLevel.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+//multilevel Inheritence
+//java does not support multiple inheritence 
+class one{
+    public void flower(){
+        System.out.println("Flower!");
+
+    } 
+}
+class two extends one{
+    public void rose(){
+        System.out.println("Rose!");
+    }
+}
+
+class three extends two{
+    public void lily(){
+        System.out.println("Lily!");
+    }
+}
+
+
+
+class MultiLevelInheritence{
+    public static void main(String[] args){
+   // three obj = new three();
+    three obj = new three();
+
+    obj.lily();
+    obj.rose();
+    obj.flower();
+    }
+}
