53d28a99fac55c331dc80586badb7cb8d8eb2928
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-26 14:58:46 +0530

    Create ArrayListExample.java

---
+++ JAVA Codes/ArrayListExample.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+//import java.util.Collections;
+
+class ArrayListExample{
+    public static void main(String[] args){
+    ArrayList<Integer> list = new ArrayList<>();
+    //add
+    list.add(0);
+    list.add(2);
+    list.add(3);
+
+    System.out.println(list);
+
+   
+
+
+}
+}
