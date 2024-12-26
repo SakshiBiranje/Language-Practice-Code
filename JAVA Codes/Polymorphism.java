bb325043c45cb0f57d625f881fbe1c185b13bc4c
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-26 16:58:42 +0530

    Create Polymorphism.java

---
+++ JAVA Codes/Polymorphism.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Helper
+{
+    static int multiply(int a , int b){
+        return a*b; 
+    }
+    static double multiply(double a , double b){
+        return a*b;
+    }
+}
+class Polymorphism{
+    public static void main(String[] args){
+        System.out.println(Helper.multiply(2,3));
+        System.out.println(Helper.multiply(5.2,3.7));
+    }
+}
