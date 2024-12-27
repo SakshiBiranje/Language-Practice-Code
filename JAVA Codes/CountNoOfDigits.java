bfb0ee0c2186e749e6c5bd9abab5de3a3fa81d45
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-27 14:33:09 +0530

    Create CountNoOfDigits.java

---
+++ JAVA Codes/CountNoOfDigits.java
@@ -0,0 +1,13 @@
+import java.util.*;
+//i/o= 12345
+// o/p =5 return no of digits
+
+class CountNoOfDigits{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        String n = sc.nextLine();
+        int a = n.length();
+        System.out.println(a);
+    }
+}
