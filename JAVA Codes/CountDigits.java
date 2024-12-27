e514298cf534da0bc11137a64d4c4ed3da99099e
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-27 14:33:07 +0530

    Create CountDigits.java

---
+++ JAVA Codes/CountDigits.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class CountDigits{
+    public static int Digits(int n){
+       int count =0;
+       int originalNumber = n;
+       while(n > 0){
+        int digit= n % 10;
+        if(digit !=0 && originalNumber % digit == 0){
+            count ++;
+        }
+        n/=10;
+       }
+       return count;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int n= sc.nextInt();
+       // int count=(int) (Math.log10(n)+1);
+       // System.out.println(count);
+       int result= Digits(n);
+       System.out.println("Result : "+ result);
+       
+    }
+}
