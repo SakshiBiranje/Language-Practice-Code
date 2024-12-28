ffd6fee01cfbbf485a3151cfd3c35049fd384ee7
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-28 16:22:22 +0530

    Create ArmStrongNum.java

---
+++ JAVA Codes/ArmStrongNum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.util.Scanner;
+public class ArmStrongNum {
+	
+	public static boolean armstrong(int n){
+		int dup = n;
+        
+        int ab= n/10;
+        int c= n%10;
+        int b = ab%10;
+        int a = ab/10;
+        int ans = a*a*a + b*b*b + c*c*c;
+        if(dup==ans){
+            return true;
+        }
+        else{
+            return false;
+        }
+      
+        
+	}
+	public static void main(String[] args) {
+		// Write your code here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+        boolean ans= armstrong(n);
+		System.out.println(ans);
+
+
+	}
+}
