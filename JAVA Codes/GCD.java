e40951099efafc7451c9aa38a76178d89980f8b6
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-28 15:37:27 +0530

    Create GCD.java

---
+++ JAVA Codes/GCD.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class GCD{
+
+    public static int GCDFinder(int a , int b){
+        int ans=0;
+        if(a>b){
+            for(int i=0; i<=b;i++){
+           a=a%b;
+           if(a!=0){
+            ans =a;
+            } else{
+            ans =b;
+           }
+           
+            }
+        }
+        else{
+            for(int i=0; i<=a;i++){
+           
+           b=b%a;
+           if(b!=0){
+            ans =b;
+           }
+           else{
+            ans =a;
+           }
+           
+            }
+        }
+              
+        return ans;
+
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers : ");
+        int a= sc.nextInt();
+        int b = sc.nextInt();
+
+        int result = GCDFinder(a,b);
+        System.out.println(result);
+    }
+}
