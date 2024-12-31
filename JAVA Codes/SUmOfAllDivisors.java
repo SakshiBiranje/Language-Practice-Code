275aac75d0df9b6e98265b56d2232912257076e7
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-12-31 17:00:31 +0530

    Update SUmOfAllDivisors.java

--- JAVA Codes/SUmOfAllDivisors.java
+++ JAVA Codes/SUmOfAllDivisors.java
@@ -16,8 +16,8 @@ public class SUmOfAllDivisors {
        return divisors;
     }
 
-
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number : ");
         int num = sc.nextInt();
