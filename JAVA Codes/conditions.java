3df43c4f7a85465806e95175b7ad75281bc5b084
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-02 21:40:33 +0530

    Update conditions.java

--- JAVA Codes/conditions.java
+++ JAVA Codes/conditions.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class conditions 
 {
-    public static void main(String args[]) {
+    public static void main(String args[]) 
+    {
         Scanner sc = new Scanner(System.in);
 
         //adult age checker
