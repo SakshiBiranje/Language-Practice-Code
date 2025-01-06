9a6edad688de508ec8d32fae513363d043455806
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-06 21:15:34 +0530

    Update CountFrequency.java

--- JAVA Codes/CountFrequency.java
+++ JAVA Codes/CountFrequency.java
@@ -3,23 +3,18 @@ import java.util.*;
 public class CountFrequency {
  
 public static void main(String args[]){  
-   
    int arr[] = {10,5,10,15,10,5};
      int n = arr.length;
      Frequency(arr, n);
   }
-static void Frequency(int arr[], int n)
-{
+
+static void Frequency(int arr[], int n){
     Map<Integer, Integer> map = new HashMap<>();
- 
-    for (int i = 0; i < n; i++)
-    {
-        if (map.containsKey(arr[i]))
-        {
+    for (int i = 0; i < n; i++){
+        if (map.containsKey(arr[i])){
             map.put(arr[i], map.get(arr[i]) + 1);
         }
-        else
-        {
+        else{
             map.put(arr[i], 1);
         }
     }
