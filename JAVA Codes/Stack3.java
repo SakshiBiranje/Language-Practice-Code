56c1b768c950f59f9b2d2350aefb26a5552c8f6d
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-07 22:26:10 +0530

    Create Stack3.java

---
+++ JAVA Codes/Stack3.java
@@ -0,0 +1,22 @@
+import java.util.*;
+//insert at bottom of stack
+class Stack3{
+    private static void insertAtBottom(Stack<Integer> s, int data){
+       if(s.isEmpty()){
+        s.push(data);
+        return;
+       }
+       int top = s.pop();
+       insertAtBottom(s,data);
+       s.push(top);
+    }
+    public static void main(String[] args){
+        Stack <Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        insertAtBottom(s,5);
+        System.out.println(s);
+    }
+}
