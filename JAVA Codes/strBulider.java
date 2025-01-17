1956ca45cfffdaba045c84d807ec3a9f749d46a3
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-17 12:06:09 +0530

    Update strBulider.java

--- JAVA Codes/strBulider.java
+++ JAVA Codes/strBulider.java
@@ -6,7 +6,6 @@ public static void main(String args[])
 {
     StringBuilder sb = new StringBuilder("Tony");
     System.out.println(sb);
-
     //char at index 0
     System.out.println(sb.charAt(0));
      
