ab0a1d03a1fcc37510dcc9e18afacb331f23c263
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-17 12:06:19 +0530

    Update Stack1.java

--- JAVA Codes/Stack1.java
+++ JAVA Codes/Stack1.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Stack1{
     public static void main(String[] args){
         Stack<Integer> s = new Stack<>();
-       
         s.push(1);
         s.push(2);
         s.push(3);
