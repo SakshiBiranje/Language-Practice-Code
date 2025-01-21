2621a999e4300e1d1d729b031b1359f18ba4bcd7
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-21 18:52:22 +0530

    Update ForLoop.c

--- C Codes/ForLoop.c
+++ C Codes/ForLoop.c
@@ -3,15 +3,14 @@
 int main() 
 {
   int i, j;
-  // Outer loop
+  
   for (i = 1; i <= 2; ++i) 
   {
-    printf("Outer: %d\n", i);  // Executes 2 times
-    
-    // Inner loop
+    printf("Outer: %d\n", i);  
+  
     for (j = 1; j <= 3; ++j) 
     {
-      printf(" Inner: %d\n", j);  // Executes 6 times (2 * 3)
+      printf(" Inner: %d\n", j);  
     }
   }
   
