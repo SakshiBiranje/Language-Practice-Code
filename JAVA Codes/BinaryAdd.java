9e3f0c760d8b11e4b5b92ffa0444ee4e3521fb1e
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-21 18:52:28 +0530

    Create BinaryAdd.java

---
+++ JAVA Codes/BinaryAdd.java
@@ -0,0 +1,7 @@
+class BinaryAdd{
+    public static void main(String[] args){
+        int a=7;
+        int b=2;
+        System.out.println(Integer.toBinaryString(a)+ Integer.toBinaryString(b));
+    }
+}
