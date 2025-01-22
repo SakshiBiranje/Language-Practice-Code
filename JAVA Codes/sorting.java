3f611f3f0bb88c9cc5a2485ea08445bf9fd0a243
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-22 18:53:46 +0530

    Update sorting.java

--- JAVA Codes/sorting.java
+++ JAVA Codes/sorting.java
@@ -9,8 +9,7 @@ public class sorting
         System.out.println();
         
     }
-        public static void main(String args[])
-        {
+        public static void main(String args[]) {
         int arr[]={7,8,3,1,2};
     
         for(int i=0;i<arr.length-1;i++)
