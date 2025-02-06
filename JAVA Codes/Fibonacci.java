8b5f62ff47ff8d3545a0d310ffe4966a940e619d
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-06 23:40:43 +0530

    Update Fibonacci.java

--- JAVA Codes/Fibonacci.java
+++ JAVA Codes/Fibonacci.java
@@ -2,18 +2,17 @@
 
 public class fibonacci 
 {
-public static void main(String[] args) 
-{
-int n = 10, num1 = 0, num2 = 1;
+  public static void main(String[] args) 
+    {
+      int n = 10, num1 = 0, num2 = 1;
+      System.out.print("Fibonacci Series: " + num1 + "," + num2);
 
-System.out.print("Fibonacci Series: " + num1 + "," + num2);
-
-for (int i = 2; i < n; i++) 
-{
-  int num3 = num1;
-num1 = num2;
-num2 = num3;
-System.out.print(", " + num3);
+      for (int i = 2; i < n; i++) 
+        {
+            int num3 = num1;
+            num1 = num2;
+            num2 = num3;
+        System.out.print(", " + num3);
       }
     }
 }
