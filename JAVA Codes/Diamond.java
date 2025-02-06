d5886b4727c2c497c1ae890523e841d6deadf16f
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-06 23:40:38 +0530

    Update Diamond.java

--- JAVA Codes/Diamond.java
+++ JAVA Codes/Diamond.java
@@ -16,12 +16,9 @@ public class Diamond
             for(int j=1;j<=2*i-1;j++)
             {
                 System.out.print("*");
-            }
-            
+            }    
             System.out.println();
         }
-
-
         for(int i=n;i>=1;i--){
             //spaces
             for(int j=1;j<=n-i;j++){
