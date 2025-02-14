29c800b4e5733f170fef57ebf46690189880b904
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-14 20:15:46 +0530

    Update String3.java

--- JAVA Codes/String3.java
+++ JAVA Codes/String3.java
@@ -8,7 +8,7 @@ public class String3 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a ");
         String str= "a1b10";
-        for(int i=0;i<str.length();i++){
+        for(int i=0;i<str.length();i++) {
             boolean a = str. isDigit(i);
            // int a = str.charAt(i);
             System.out.println(a);
