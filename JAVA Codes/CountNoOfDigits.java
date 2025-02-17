0d33873e7cc9fae7a06b79fe4aa570b366791cb8
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-17 17:03:18 +0530

    Update CountNoOfDigits.java

--- JAVA Codes/CountNoOfDigits.java
+++ JAVA Codes/CountNoOfDigits.java
@@ -6,6 +6,7 @@ class CountNoOfDigits{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number : ");
+        
         String n = sc.nextLine();
         int a = n.length();
         System.out.println(a);
