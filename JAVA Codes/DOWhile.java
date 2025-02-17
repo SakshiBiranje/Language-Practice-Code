e9b14fde85b059b12839cd90cbfa2d9d5a144914
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-17 17:02:41 +0530

    Update DOWhile.java

--- JAVA Codes/DOWhile.java
+++ JAVA Codes/DOWhile.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class DOWhile
 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int c=1;
         do{
             System.out.println("Hello Sakshi!!!");
             c++;
         }
-        
+
         while(c<6);
     }
 }
