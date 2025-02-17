6585b52bf28fb12288125df5c22d948bd39a0700
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-17 17:03:48 +0530

    Update BreakLoop.java

--- JAVA Codes/BreakLoop.java
+++ JAVA Codes/BreakLoop.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class BreakLoop{
     public static void main(String[] args){
-        for(int i= 0;i<9;i++){
+        for(int i= 0;i<9;i++ ) {
             if(i==5)
             break;
             System.out.println("Hello " + i);
