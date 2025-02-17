9980d720ee50b8a32271949a8e87bedddbfee9a9
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-17 17:03:40 +0530

    Update BreakStatement.java

--- JAVA Codes/BreakStatement.java
+++ JAVA Codes/BreakStatement.java
@@ -4,7 +4,7 @@ class BreakStatement{
     // using switch ,case, break keywords
     public static void main(String[] args){
     int n=1;
-    switch(n){
+    switch(n) {
         case 1 : 
         System.out.println("Sakshi");
         break;
