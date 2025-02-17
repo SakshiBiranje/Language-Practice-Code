1fd8bd72c72aed4f793e753f6dbd2068c3361477
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-17 17:03:09 +0530

    Update Diamond.java

--- JAVA Codes/Diamond.java
+++ JAVA Codes/Diamond.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Diamond 
 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         int n = 4;
 
         for(int i=1;i<=n;i++)
