83763fc0a7e223fbb47ea01fa5945252c65647da
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-18 16:49:14 +0530

    Update ArmStrongNum.java

--- JAVA Codes/ArmStrongNum.java
+++ JAVA Codes/ArmStrongNum.java
@@ -2,22 +2,19 @@ import java.util.*;
 import java.util.Scanner;
 public class ArmStrongNum {
 	
-	public static boolean armstrong(int n){
+	public static boolean armstrong(int n) {
 		int dup = n;
-        
         int ab= n/10;
         int c= n%10;
         int b = ab%10;
         int a = ab/10;
         int ans = a*a*a + b*b*b + c*c*c;
-        if(dup==ans){
+        if(dup==ans) {
             return true;
         }
         else{
             return false;
-        }
-      
-        
+        }  
 	}
 	public static void main(String[] args) {
 		
