3346babd2643e403b574eb32746e69da99467bf6
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-24 23:35:15 +0530

    Update output.java

--- JAVA Codes/output.java
+++ JAVA Codes/output.java
@@ -5,6 +5,7 @@ public class output
     public static void main(String[] arg)
     {
         Scanner sc = new Scanner(System.in);
+        
         String name = sc.next();
         System.out.println(name);
 
