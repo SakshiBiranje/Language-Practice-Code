10e826a072ef9a2bdfb3d27929c84d5bbe496e9d
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-24 23:27:36 +0530

    Update Palindrome.java

--- JAVA Codes/Palindrome.java
+++ JAVA Codes/Palindrome.java
@@ -6,6 +6,5 @@ public static void main(String[] args)
 String str = "madam";
 
 String reversed = new StringBuilder(str).reverse().toString(); System.out.println(str.equals(reversed));
-
-}
+    }
 }
