c9edfee10e4c1df245314b61fc2d851e16fbb800
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-24 23:27:48 +0530

    Update patterns.java

--- JAVA Codes/patterns.java
+++ JAVA Codes/patterns.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class patterns {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-   int n=4;
+        int n=4;
    for(int i=1;i<=n;i++){
     for(int j=1;j<=n-i;j++){
     System.out.print(" ");
