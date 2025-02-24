9984e25a314ae0ba73bf9a9b374a15e9a87f2882
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-24 23:27:11 +0530

    Update Palindromenumber.java

--- JAVA Codes/Palindromenumber.java
+++ JAVA Codes/Palindromenumber.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Palindromenumber {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         int n =5;
         for(int i=1;i<=n;i++){
             //spaces
