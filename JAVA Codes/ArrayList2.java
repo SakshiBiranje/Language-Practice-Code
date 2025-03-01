74731ef9b99932f30123b1b9e7f2ec7115c1fe0f
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-03-01 22:18:35 +0530

    Update ArrayList2.java

--- JAVA Codes/ArrayList2.java
+++ JAVA Codes/ArrayList2.java
@@ -6,12 +6,13 @@ public class ArrayList2 {
     public static void main(String[] args) {
         
         List<String> list = new ArrayList<>();
-        
+
         list.add("Orange");
         list.add("White");
         list.add("Red");
         list.add("Blue");
         list.add("pink");
+        
         System.out.println("Before sorting : " + list);
         Collections.sort(list);
         System.out.println("Sorted list of colors: " + list);
