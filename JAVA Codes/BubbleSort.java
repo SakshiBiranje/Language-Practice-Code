148939d795ab557140d5d2fea08c4cbd0b928f97
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-03-04 22:20:08 +0530

    Update BubbleSort.java

--- JAVA Codes/BubbleSort.java
+++ JAVA Codes/BubbleSort.java
@@ -8,9 +8,9 @@ public class BubbleSort
         }
         System.out.println();
     }
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         int arr[]={7,8,3,1,2};
-
         //bubble sort
         //time complexity = O(n^2) so it is a bad time complexity
         for(int i=0 ; i<arr.length-1; i++){
